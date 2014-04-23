e36ad465dbbf10958902d63a4754659b3f6f47e6
qianfeng <apple@aofel.local>
2014-04-23 15:56:58 +0800

    Initial Commit

---
+++ SnsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SnsDemo
+//
+//  Created by qianfeng on 14-4-23.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
